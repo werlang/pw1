648ebaff6e750f660514c309813af5c1bfdeede3
werlang <pswerlang@gmail.com>
2022-10-26 13:50:18 +0000

    Add sample.

---
+++ exemplos/ex08.1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="info.php" method="GET">
+        <label>Nome:<input name="nome"></label>
+        <label>Email:<input name="email"></label>
+        <button>Enviar</button>
+    </form>
+</body>
+</html>
