1d74d2590b3b977cd92ab53e4a0b6acc41b839ee
werlang <pswerlang@gmail.com>
2022-11-30 20:06:20 +0000

    Change to simplified sample.

--- exemplos/ex10.1/getusers.php
+++ exemplos/ex10.1/getusers.php
@@ -2,27 +2,10 @@
 require "connection.php";
 
 $output = [];
+$id = $_GET["user"];
+$sql = "SELECT * FROM users WHERE id = $id";
+$stmt = $conn->query($sql);
+$output["result"] = $stmt->fetch();
+$output["status"] = "OK";
 
-if (!isset($_GET["user"])) {
-    $output["status"] = "error";
-    $output["message"] = "Must inform user id";
-}
-else {
-    $id = $_GET["user"];
-
-    try {
-        $sql = "SELECT * FROM users WHERE id = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([ $id ]);
-        $output["result"] = $stmt->fetch();
-        $output["status"] = "OK";
-    }
-    catch(PDOException $error) {
-        echo json_encode([
-            "status" => "error",
-            "message" => "Failed to get user info.",
-            "error" => $error
-        ]);
-    } 
-}
 echo json_encode($output);
