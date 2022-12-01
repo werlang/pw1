d6c444d6f84d9c134d4ac89d7b73eaecd8a3b790
werlang <pswerlang@gmail.com>
2022-12-01 20:16:24 +0000

    Update sample.

--- exemplos/ex10.1/index.js
+++ exemplos/ex10.1/index.js
@@ -1,7 +1,11 @@
 import { request } from './utils.js';
 
-async function main() {
-    const data = await request(`getusers.php`, { user: 1 });
-    console.log(data);
-}
-main();
+// async function main() {
+//     const data = await request(`getusers.php`, { user: 1 });
+//     console.log(data);
+// }
+// main();
+
+request(`getusers.php`, { user: 1 }).then( data => {
+    console.log(data)
+})
