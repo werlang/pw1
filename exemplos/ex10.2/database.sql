22a6ba5ee56b6dee5f66b2dacc3ad374f0e67e2c
werlang <pswerlang@gmail.com>
2022-12-16 11:18:46 +0000

    Add database script.

---
+++ exemplos/ex10.2/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8mb4;
