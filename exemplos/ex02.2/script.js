9762d0997614bd717a8d7f12eb4bf6bd259042f6
Pablo Werlang <pswerlang@gmail.com>
2023-04-14 12:22:46 -0300

    Update sample

--- exemplos/ex02.2/script.js
+++ exemplos/ex02.2/script.js
@@ -1,14 +1,13 @@
-function bind(element) {
-    element.addEventListener('input', () => {
-        const n1 = document.querySelector('#n1');
-        const n2 = document.querySelector('#n2');
-        const result = parseInt(n1.value) * parseInt(n2.value);
-        if (isNaN(result)){
-            document.querySelector('#result').innerHTML = 'ERRO';
-            return;
-        }
-        document.querySelector('#result').innerHTML = result;
-    });
+const n1 = document.querySelector('#n1');
+const n2 = document.querySelector('#n2');
+const result = document.querySelector('#result');
+
+function multiplica() {
+    const vn1 = parseInt(n1.value);
+    const vn2 = parseInt(n2.value);
+    const mult = vn1 * vn2;
+    result.innerHTML = mult;
 }
-bind(document.querySelector('#n1'));
-bind(document.querySelector('#n2'));
+
+n1.addEventListener('input', multiplica);
+n2.addEventListener('input', multiplica);
