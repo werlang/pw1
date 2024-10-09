4693ed180a55810ce8a5be886e82adf6c98becc7
Pablo Werlang <pswerlang@gmail.com>
2024-10-09 15:04:04 -0300

    .

--- exercicios/ex09.2/script.js
+++ exercicios/ex09.2/script.js
@@ -1,8 +1,10 @@
-async function request() {
-    const res = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
-    const data = await res.json();
-    document.querySelector('#price').innerHTML = '$' + parseFloat(data.price).toFixed(1);
-    setTimeout(() => request(), 5000);
-}
+const price = document.querySelector('#price');
 
-request();
+async function showPrice() {
+    const url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT';
+    const data = await fetch(url).then(res => res.json())
+    price.innerHTML = '$' + parseFloat(data.price).toFixed(2);
+}
+showPrice();
+
+setInterval(showPrice, 5000);
