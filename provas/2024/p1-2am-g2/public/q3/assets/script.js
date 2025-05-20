7db24f7fa984c877bae762a53bc5bf8472b2dadb
Pablo Werlang <pswerlang@gmail.com>
2025-05-19 22:09:25 -0300

    add test

---
+++ provas/2024/p1-2am-g2/public/q3/assets/script.js
@@ -0,0 +1,12 @@
+function showProblem() {
+    // Implemente a função showProblem aqui
+}
+
+// teste da função
+const testProblem = {
+    title: 'TÍTULO',
+    description: 'DESCRIÇÃO',
+    input: 'ENTRADA',
+    output: 'SAÍDA'
+}
+showProblem(testProblem);
