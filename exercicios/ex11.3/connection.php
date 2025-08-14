70e6a1b6f4a6d8f95fb4fb81f592d0d39c1af835
Pablo Werlang <pswerlang@gmail.com>
2025-08-13 21:16:25 -0300

    Rename folders.

--- exercicios/ex11.3/connection.php
+++ exercicios/ex11.3/connection.php
@@ -1,4 +1,5 @@
 <?php
+
 $host = "mysql";
 $user = "root";
 $password = "asdf1234";
@@ -8,8 +9,9 @@ $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
+
 $conn = new PDO(
-    "mysql:host=$host;dbname=$database;port=$port",
+    "mysql:host=$host;port=$port;dbname=$database",
     $user,
     $password,
     $options
