0c4e0d838a0b7f6af349bb9423845dbf340adfaa
Pablo Werlang <pswerlang@gmail.com>
2026-03-16 10:24:47 -0300

    Refactor spaceship game: update CSS for background and image, enhance JavaScript for movement controls and boundary restrictions

--- public/01-eventos/spaceship/index.js
+++ public/01-eventos/spaceship/index.js
@@ -1,23 +1,49 @@
-const spaceship = document.querySelector('img');
+const spaceship = document.querySelector('#spaceship');
 
-const increment = 10; // pixels to move per key press
+let x = 230;
+let y = 440;
 
-let x = 200;
-let y = 380;
-spaceship.style.position = 'absolute';
-spaceship.style.left = x + 'px';
-spaceship.style.top = y + 'px';
+const size_W = 500;
+const size_H = 500;
+const spaceship_W = 40;
+const spaceship_H = 40;
 
-document.body.addEventListener('keydown', (event) => {
-    if (event.key === 'a') {
-        x -= increment;
-    } else if (event.key === 'd') {
-        x += increment;
-    } else if (event.key === 'w') {
-        y -= increment;
-    } else if (event.key === 's') {
-        y += increment;
+moveSpaceship(x,y);
+const speed = 10;
+
+document.body.addEventListener('keydown', (e) => {
+    // atualiza x e y de acordo com as teclas pressionadas
+    if (e.key == 'w') {
+        y -= speed;
     }
+    else if (e.key == 's') {
+        y += speed;
+    }
+    else if (e.key == 'a') {
+        x -= speed;
+    }
+    else if (e.key == 'd') {
+        x += speed;
+    }
+
+    // restrições da tela
+    if (x < 0) {
+        x = 0;
+    }
+    else if (x > size_W - spaceship_W) {
+        x = size_W - spaceship_W;
+    }
+    if (y < 0) {
+        y = 0;
+    }
+    else if (y > size_H - spaceship_H) {
+        y = size_H - spaceship_H;
+    }
+
+    moveSpaceship(x,y);
+});
+
+function moveSpaceship(x, y) {
     spaceship.style.left = x + 'px';
     spaceship.style.top = y + 'px';
-});
+}
